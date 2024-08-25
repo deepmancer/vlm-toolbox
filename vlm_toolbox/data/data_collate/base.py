feccfd0b3bdcf3fa1cb4eed72162403d1f832b2d
alirezaheidarics <alireza_heidari@sfu.ca>
2024-08-25 17:26:53 +0330

    remove legacy codes for data loading

--- vlm_toolbox/data/data_collate/base.py
+++
@@ -1,9 +0,0 @@
-from transformers import DefaultDataCollator
-
-
-class BaseDataCollator(DefaultDataCollator):
-    def get_label_names(self):
-        raise NotImplementedError
-    
-    def __call__(self, *args, **kwargs):
-        raise NotImplementedError
